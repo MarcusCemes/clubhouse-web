077a5b6de00e2e4c6842e2a0f70a6ff8979b9334
Marcus Cemes <marcus.cemes@gmail.com>
2022-04-29 14:23:38 +0200

    fix: correct <Nav /> logo size

--- src/lib/components/layout/nav/Nav.svelte
+++ src/lib/components/layout/nav/Nav.svelte
@@ -8,7 +8,7 @@
 <nav class="h-24 flex justify-center border-b">
     <div class="w-full max-w-5xl flex items-center">
         <a href="/" class="relative bottom-0.5 text-2xl font-bold select-none">
-            <Logo class="h-16" />
+            <Logo class="h-12" />
         </a>
 
         <span class="ml-12 inline-flex items-center font-semibold">
